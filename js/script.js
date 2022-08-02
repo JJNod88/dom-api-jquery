99e1c36373aea25fbf61be24dd6333ec931e482d
JJNod88 <j.noddings@hotmail.com>
2022-08-02 20:35:10 +0000

    jquery

--- js/script.js
+++ js/script.js
@@ -1,20 +1,55 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
-   });
-   $("#stream2_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream2").addClass('highlight_stream');
-   });
-   $("#stream3_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream3").addClass('highlight_stream');
-   });
-}); 
+     $(".box").on("click", function() {
+          /**
+           * When we click on an element that has
+           * a 'box' class, this event will be fired.
+           */
+          var classNames = $(this).attr("class").split(" ");
+          var boxClass = classNames[0];
+          var className = classNames[1];
+          if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+               // If this object is already red, turn it black
+               $("." + className).css("background-color", "#000"); 
+          } else {
+               // Else turn all elements with a box class black
+               // and then change the color of all elements
+               // with the same class name as the clicked element
+               // to red.
+               $("." + boxClass).css("background-color", "#000");
+               $("." + className).css("background-color", "red");
+          }
+     });
+ })
+
+/** 
+ $(document).ready(function() {
+     $('.box').on("click", function (){
+          var classNames = $(this).attr("class").split(" ");
+          $("." + classNames[1]).css('background-color', 'red')
+     })
+})
+
+*/
+
+
+// classes to change color of button
+
+/** 
+
+$('button').mouseenter(function() {
+     $(this).removeClass("makeRed").addClass("makeBlue")
+})
+
+$('button').mouseleave(function() {
+     $(this).removeClass("makeBlue").addClass("makeRed")
+})
+
+*/
+
+
+// using this to hide the element yu clicked on
+
+/** $('p').click(function() {
+     $(this).slideToggle('slow')
+})
+*/ 
--- js/statements.js
+++ js/statements.js
@@ -36,3 +36,114 @@ $("a:last");
 
 // Select all header elements (h1, h2, h3, h4, h5, h6)
 $(":header");
+
+
+
+
+ /** ALL PREVIOUS CODING FOR TIPS
+
+$(document).ready(function() {
+    $("#stream1_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+}); 
+
+
+//EVENTS-------
+
+//CI version 
+
+$('p').click(function(){
+	$('p').css('color', 'red');
+});
+
+$('h2').hover(function(){
+	$('h2').css('background', 'lightblue');
+});
+
+$('.card-panel').mouseenter(function() {
+    $('body').css("background-color", "black");
+})
+
+$('.card-panel').mouseleave(function() {
+    $('body').css("background-color", "#e1e2e2");
+})
+
+
+// CHAINING
+
+$('button').mouseenter(function() {
+     $('button').removeClass("make-red").addClass("make-border")
+ });
+ 
+ $('button').mouseleave(function() {
+     $('button').removeClass("make-border").addClass("make-red")
+ });
+
+
+// My version 
+
+$('#container').on("click", function() {
+     $('p').css("color", "red");
+ });
+ 
+ $('h2').hover(function() {
+     $('h2').addClass("hover-class");
+     }, function() {
+     $('h2').removeClass("hover-class");
+     });
+     
+ $('.card-panel').mouseenter(function() {
+     $('body').css("background-color", "black");
+ })
+ 
+ $('.card-panel').mouseleave(function() {
+     $('body').css("background-color", "#e1e2e2");
+ })
+
+
+// EFFECTS -----------
+
+ $('#card-panel-1').click(function(){
+     $('#card-panel-1').hide('slow');
+ });
+ 
+ $('#card-panel-2').click(function(){
+     $('#card-panel-2').hide('medium');
+ });
+ 
+ $('#card-panel-3').click(function(){
+     $('#card-panel-3').hide('fast');
+ })
+ 
+ $('#card-panel-4').click(function(){
+     $('#card-panel-4').hide('3000');
+ })
+ 
+
+ * $('document').ready(function() {
+ * })
+ * 
+ *$('header').css('background-color', 'red');
+ * $('header').html("<h1>New Header</h1>");
+ * $('header').text('Change text');
+ * $('header').append('<p>New P text</p>);
+ 
+ 
+ *
+ */
